d60175e4a2d13d23f5e9abb44f774f00bc2deab4
FelipeLL <f3lip3.3z@hotmail.com>
2022-05-08 20:48:13 -0500

    prueba mapbox fix N_9

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -9,12 +9,12 @@ import Admin from "./Admin";
 import { UserContext } from "../context/UserProvider";
 import Logout from "./Logout";
 import mapboxgl from "mapbox-gl";
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const MapView = () => {
-  // @ts-ignore
-  // eslint-disable-next-line import/no-webpack-loader-syntax
-  mapboxgl.workerClass =
-    require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
   //estado inicial de la vista
   const initialState = {
     longitude: -74.3464647,
